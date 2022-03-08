3f80935583c7c9943268435771c22022daa68784
Brian Bui <bbui195@gmail.com>
2022-03-07 16:48:00 -0800

    added more methods to game and player classes

--- ghost/game.rb
+++ ghost/game.rb
@@ -3,7 +3,7 @@ require "set"
 
 class Game
 
-    attr_reader :current_player
+    attr_reader :current_player, :fragment
 
     def initialize(player1, player2, dictionary)
         @player1 = player1
@@ -12,10 +12,53 @@ class Game
         @previous_player = nil
         @fragment = ""
         @dictionary = dictionary
+        @losses = Hash.new(0)
+    end
+
+    def record(player)
+        "GHOST"[0...@losses[player]]
+    end
+
+    def is_over?
+        @losses[@player1] == 5 || @losses[@player2] == 5
+    end
+
+    def display_standings
+        puts "#{@player1.name}: #{record(@player1)}"
+        puts "#{@player2.name}: #{record(@player2)}"
+    end
+
+    def run
+        while !is_over?
+            display_standings
+            while !lose?
+                puts "The current fragment is #{@fragment}"
+                play_round
+            end
+            puts "#{@current_player.name} loses!"
+            @fragment = ""
+            next_player!
+        end
+        display_standings
+        next_player!
+        puts "#{@current_player.name} is the ultimate loser!"
     end
 
     def play_round
-        
+        guess = @current_player.guess_char
+        if valid_play?(guess)
+            @fragment += guess
+            if lose?
+                @losses[@current_player] += 1
+            else
+                next_player!
+            end
+        else
+            puts "Invalid guess"
+            raise "Invalid guess"
+        end
+    rescue
+        retry
     end
 
     def next_player!
@@ -37,6 +80,12 @@ class Game
         return @dictionary.any? { |el| el.start_with?(new_fragment) }
     end
 
+    def lose?
+        @dictionary.include?(fragment)
+    end
+
+
+
 end
 
 
@@ -50,3 +99,4 @@ player2 = Player.new('Ernesta')
 
 game = Game.new(player1, player2, dictionary)
 
+game.run
--- ghost/player.rb
+++ ghost/player.rb
@@ -2,12 +2,14 @@
 
 class Player
 
+    attr_reader :name
+
     def initialize(name)
         @name = name
     end
 
     def guess_char
-        puts "#{name}, please add a character"
+        puts "#{@name}, please add a character"
         response = gets.chomp
         response
     end
